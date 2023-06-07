23e25286aa437227e68f980a33645ce621899606
marcosm4all <marcos@media4all.com.br>
2023-06-06 21:26:43 -0300

    adicionando codigo de matson e Arlley (sugestao)

--- image.py
+++ image.py
@@ -1,8 +1,6 @@
-import cv2, numpy
+import cv2, numpy, time
 
-corrida_inicio = cv2.imread("imagensTeste/corrida_inicio.jpeg")
 corrida_meio = cv2.imread("imagensTeste/corrida_meio.jpeg")
-todoCorpo = cv2.imread("imagensTeste/todo_corpo.jpeg")
 
 imageIn = corrida_meio
 
@@ -14,17 +12,26 @@ grey = cv2.cvtColor(imageIn, cv2.COLOR_BGR2GRAY )
 human_head = human_head_cascade.detectMultiScale(grey, 1.2, 5)
 
 contador = 0
+
 for (x,y,w,h) in human_head:
     print(x,y,w,h)
+    contador += 1
+    #codigo de Matson para identificar os rostos. dica : Usar um mapping rostost = {(id, coordenada), (id, coordenada)} 
+    titulo = str(contador) + " Rosto"
     cv2.rectangle(imageIn, (x,y), (x+w, y+h), (255,0,0), 2)
-    cv2.putText(imageIn, 'Rosto ', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255,0,0), 2)
+    cv2.putText(imageIn, titulo, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255,0,0), 2)
 
+print("Rostos selecionados:")
+time.sleep(5)
 
+while True:
+    cv2.imshow('Escolha o Rosto',imageIn)
+    
+    if (cv2.waitKey(1) & 0xFF) == ord('q'):
+        break
+
+escolha = input("Selecione um rosto para não esconder: ")
+#Código de Arley aqui pra borrar os rostos com as coordenadas 
 
 cv2.imwrite("resultado/detectface3.jpg", imageIn) 
 
-""" 
-upper_human = human_upper_boddy_cascade.detectMultiScale(grey, 1.1, 1)
-human = human_full_boddy_cascade.detectMultiScale(grey, 1.1, 1)
-human_upper_boddy_cascade = cv2.CascadeClassifier('xmlFiles/haarcascade_upperbody.xml')
-human_full_boddy_cascade = cv2.CascadeClassifier('xmlFiles/haarcascade_fullbody.xml') """
